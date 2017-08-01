24ee7d543951fb8f603bf2a4903f28b791e4d25d
CaberoDaniel <Daniel.Cabero@fundacion-jala.org>
2017-08-01 09:43:05 +0000

    travis config

--- Rakefile
+++ Rakefile
@@ -1,28 +1,28 @@
-# require 'bundler/gem_tasks'
-# require 'rspec/core/rake_task'
-#
-# RSpec::Core::RakeTask.new(:spec)
-#
-# task :smoke do
-#   exec "spec/smoke"
-# end
-#
-# namespace :dummy do
-#   require_relative "features/support/env"
-# end
-#
-# task(:spec).clear
-# desc "Run specs other than spec/acceptance"
-# RSpec::Core::RakeTask.new("spec") do |task|
-#   task.exclude_pattern = "spec/acceptance/**/*_spec.rb"
-#   task.verbose = false
-# end
-#
-# desc "Run acceptance specs in spec/acceptance"
-# RSpec::Core::RakeTask.new("spec:acceptance") do |task|
-#   task.pattern = "spec/acceptance/**/*_spec.rb"
-#   task.verbose = false
-# end
+require 'bundler/gem_tasks'
+require 'rspec/core/rake_task'
 
-# desc "Run the specs and acceptance tests"
-# task default: %w(spec spec:acceptance)
+RSpec::Core::RakeTask.new(:spec)
+
+task :smoke do
+  exec 'spec/smoke'
+end
+
+namespace :dummy do
+  require_relative 'features/support/env'
+end
+
+task(:spec).clear
+desc 'Run specs other than spec/acceptance'
+RSpec::Core::RakeTask.new('spec') do |task|
+  task.exclude_pattern = 'spec/acceptance/**/*_spec.rb'
+  task.verbose = false
+end
+
+desc 'Run acceptance specs in spec/acceptance'
+RSpec::Core::RakeTask.new('spec:acceptance') do |task|
+  task.pattern = 'spec/acceptance/**/*_spec.rb'
+  task.verbose = false
+end
+
+desc 'Run the specs and acceptance tests'
+task default: 'spec spec:acceptance'
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,37 +1,31 @@
-# require 'pathname'
-# require 'yaml'
-#
-#
-# def find_config_file(filename)
-#   root = Pathname.pwd
-#   while not root.root?
-#     root.find do |path|
-#       if path.file? and path.basename.to_s == filename
-#         return path.to_s
-#       end
-#     end
-#     root = root.parent
-#   end
-#   raise 'Configuration file ' #{filename}' not found!'
-# end
-#
-# def load_app_config_file(filename)
-#   config_file = find_config_file(filename)
-#   config = YAML.load_file(config_file)
-#   $app_context = config['app']['rootPath']
-#   return config
-# end
-#
-#
-# def AfterConfiguration  config
-#   # read config file
-#   config = load_app_config_file('env.yml')
-#   # Load application config parameters
-#   $app_user = config['app']['admin_user']
-#   $app_pass = config['app']['admin_pass']
-#   $app_host = config['app']['host']
-#   $app_port = config['app']['port']
-#   $app_root = config['app']['rootPath']
-#   $app_Exchange = config['app']['Exchange-Credentials']
-#   $app_Credentials = config['app']['Credentials']
-# end
+require 'pathname'
+require 'yaml'
+
+def find_config_file(filename)
+  root = Pathname.pwd
+  until root.root?
+    root.find do |path|
+      return path.to_s if path.file? && path.basename.to_s.equal?(filename)
+    end
+    root = root.parent
+  end
+  raise 'Configuration file '
+end
+
+def load_app_config_file(filename)
+  config_file = find_config_file(filename)
+  config = YAML.load_file(config_file)
+  @app_context = config['app']['rootPath']
+  config
+end
+
+def afte_config(config)
+  # read config file
+  config = load_app_config_file('env.yml')
+  # Load application config parameters
+  @app_user = config['app']['admin_user']
+  @app_pass = config['app']['admin_pass']
+  @app_host = config['app']['host']
+  @app_port = config['app']['port']
+  @app_root = config['app']['rootPath']
+end
