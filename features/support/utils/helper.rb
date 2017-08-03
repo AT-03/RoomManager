61d7ca6d4485fbec357e19874c230f31db0ae44e
daniel-m10 <daniel.montecinos@fundacion-jala.org>
2017-08-03 11:26:58 -0400

    added method to encode the credentials in 64 base

--- features/support/utils/helper.rb
+++ features/support/utils/helper.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'base64'
 
 # Helper class.
 # Author: Daniel Montecinos, Pablo Ramirez.
@@ -9,5 +10,9 @@ class Helper
 
   def self.get_json_value(json, key)
     JSON.parse(json).fetch(key)
+  end
+
+  def self.encode_credentials(credentials)
+    Base64.encode64(credentials)
   end
 end
