79062f848d5bc13339585871d46ce89ad86b4cfe
jpaitken <juan.aitken@fundacion-jala.org>
2017-08-16 20:37:30 -0400

    Report structure added Feature improvements

--- lib/pretty_face/formatter/report.rb
+++ lib/pretty_face/formatter/report.rb
@@ -21,9 +21,7 @@ module PrettyFace
 
       def table_image_for(status, source=nil)
         var= File.dirname(__dir__)+"/templates"
-        # puts "REAL DIRECTORY>>#{File.dirname(__dir__)+"/templates"}"
         dir = "#{directory_prefix_for(source)}images"
-        # puts "<<<<<DIRECTORY>>#{dir}"
         "<img src=\"#{var}/table_#{status}.png\" alt=\"#{status}\" title=\"#{status}\">"
 
       end
